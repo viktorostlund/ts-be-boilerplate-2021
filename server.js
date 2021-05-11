7fb2b4f271d964572638afa336cec24aef814856
Viktor Östlund <viktorostlund@hotmail.com>
2021-05-11 18:15:01 +0200

    add get age query and create person mutation

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ var { buildSchema } = require('graphql');
 var pgp = require('pg-promise')(/* options */)
 var db = pgp('postgres://viktorostlund:password@localhost:5432/test_db')
 
-const getData = (name) => {
+const createPerson = (name) => {
+  // Create Person in local PostgresQL database
+  return db.one(`INSERT INTO person (person_uid, first_name, last_name, gender, date_of_birth, country_of_birth) VALUES (uuid_generate_v4(), '${name}', 'Karlsson', 'Female', '1936-01-23', 'Sweden') RETURNING *;`)
+    .then(function (data) {
+      return data;
+    })
+    .catch(function (error) {
+      return error.message
+    })
+}
+
+const getCountry = (name) => {
   // Get data from local PostgresQL database
   return db.one(`SELECT country_of_birth FROM person WHERE first_name = '${name}';`)
     .then(function (data) {
@@ -13,19 +24,49 @@ const getData = (name) => {
     .catch(function (error) {
       return error.message
     })
-} 
+}
+
+const getAge = (name) => {
+  // Get data from local PostgresQL database
+  return db.one(`SELECT date_of_birth FROM person WHERE first_name = '${name}';`)
+    .then(function (data) {
+      return new Date(Date.now() - data.date_of_birth).getFullYear();
+    })
+    .catch(function (error) {
+      return error.message
+    })
+}
  
 /// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-type Query {
-  getCountry(name: String): String
-}
+  type Mutation {
+    createPerson(name: String): Person
+  }
+  type Query {
+    country(name: String): String
+    age(name: String): String
+  }
+  type Person {
+    id: Int
+    first_name: String
+    last_name: String
+    email: String
+    date_of_birth: String
+    country_of_birth: String
+    car_uid: String
+  }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-getCountry: ({name}) => {
-  return getData(name);
+createPerson: ({name}) => {
+  return createPerson(name);
+},
+country: ({name}) => {
+  return getCountry(name);
+},
+age: ({name}) => {
+  return getAge(name);
 },
 };
  
