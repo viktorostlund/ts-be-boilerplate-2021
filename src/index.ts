1278a7e3d2c7fb3a73f09aaa4673cdd334fd3d16
Viktor Östlund <viktorostlund@hotmail.com>
2021-05-15 21:51:36 +0200

    add morgan logger and make it print graphql info

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import logger from 'morgan';
 import dotenv from 'dotenv';
 import express from 'express';
 import pgp from 'pg-promise'
 import { ApolloServer } from 'apollo-server-express';
 
+import { loggerParser } from './utils';
 import resolvers from './resolvers';
 import schema from './schema';
 
@@ -13,9 +15,11 @@ dotenv.config({
 
 export const db = pgPromise(process.env.DB_URL!)
 
-const server = new ApolloServer({ typeDefs: schema, resolvers });
+const server = new ApolloServer({ typeDefs: schema, resolvers});
 
 const app = express();
+logger.token('graphql-query', loggerParser);
+app.use(logger(':url :status :response-time :graphql-query'));
 server.applyMiddleware({ app });
 
 app.listen({ port: process.env.PORT || 4000 }, () =>
---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export const loggerParser = (req: any) => {
+    const {query, variables, operationName} = req.body;
+    if (operationName === 'IntrospectionQuery') return
+    return `GRAPHQL: \nOperation Name: ${operationName} \nQuery: ${query} \nVariables: ${JSON.stringify(variables)}`;
+  }
+  
