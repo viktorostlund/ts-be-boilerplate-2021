f759cce7846da9dfa8102aed0f5eee6b74f63adc
Viktor Östlund <viktorostlund@hotmail.com>
2021-05-15 09:30:10 +0200

    add eslint, adjust scripts, refactor

--- src/app.ts
+++ src/app.ts
@@ -2,28 +2,26 @@ import dotenv from 'dotenv';
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
+import pgp from 'pg-promise'
 
-const pgp = require('pg-promise')(/* options */)
-
-// load the environment variables from the .env file
 dotenv.config({
   path: '.env'
 });
 
+const pgPromise = pgp()
 const { DB_USERNAME, DB_PASSWORD, DB,  DB_URL, DB_PORT } = process.env
-
-const db = pgp(`postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_URL}:${DB_PORT}/${DB}`)
+const db = pgPromise(`postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_URL}:${DB_PORT}/${DB}`)
 
 interface Person {
-  first_name: String,
-  last_name: String,
-  gender: String,
-  date_of_birth: String,
-  country_of_birth: String
+  first_name: string,
+  last_name: string,
+  gender: string,
+  date_of_birth: string,
+  country_of_birth: string
 }
 
+// Create Person in DB
 const createPerson = async ({first_name, last_name, gender, date_of_birth, country_of_birth}: Person) => {
-  // Create Person in DB
   try {
     return await db.one(`INSERT INTO person (person_uid, first_name, last_name, gender, date_of_birth, country_of_birth) VALUES (uuid_generate_v4(), '${first_name}', '${last_name}', '${gender}', '${date_of_birth}', '${country_of_birth}') RETURNING *;`)
   } catch (error) {
@@ -32,11 +30,11 @@ const createPerson = async ({first_name, last_name, gender, date_of_birth, count
   }
 }
 
-const getPerson = async (name: String) => {
-  // Get Person from DB
+// Get Person from DB
+const getPerson = async (name: string) => {
   try {
     return await db.one(`SELECT * FROM person WHERE first_name = '${name}' LIMIT 1;`)
-  } catch (error) {
+  } catch (error) {
     console.error(error)
     return error.message
   }
@@ -73,7 +71,7 @@ const root = {
   createPerson: (props: Person) => {
     return createPerson(props);
   },
-  getPerson: ({name}: {name: String}) => {
+  getPerson: ({name}: {name: string}) => {
     return getPerson(name);
   },
 };
@@ -84,5 +82,6 @@ app.use('/graphql', graphqlHTTP({
   rootValue: root,
   graphiql: true,
 }));
-app.listen(4000);
+const port = process.env.PORT || 4000
+app.listen(port);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
