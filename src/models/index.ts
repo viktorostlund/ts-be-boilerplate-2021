09a6f4997666cfd43c0a97682135b34225633527
Viktor Östlund <viktorostlund@hotmail.com>
2021-05-28 16:37:43 +0200

    refactor models queries

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,7 +3,10 @@ import { Person, Car } from '../types/types';
 
 export const createPerson = ({first_name, last_name, gender, date_of_birth, country_of_birth}: Person) => {
   try {
-    return db.query(`INSERT INTO person (person_uid, first_name, last_name, gender, date_of_birth, country_of_birth) VALUES (uuid_generate_v4(), '${first_name}', '${last_name}', '${gender}', '${date_of_birth}', '${country_of_birth}') RETURNING *;`)
+    return db.query(
+      'INSERT INTO person (person_uid, first_name, last_name, gender, date_of_birth, country_of_birth) VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5) RETURNING *;',
+      [first_name, last_name, gender, date_of_birth, country_of_birth]
+    )
   } catch (error) {
     console.error(error)
     return error.message
@@ -12,7 +15,10 @@ export const createPerson = ({first_name, last_name, gender, date_of_birth, coun
 
 export const createCar = ({make, model, price}: Car) => {
   try {
-    return db.query(`INSERT INTO car (car_uid, make, model, price) VALUES (uuid_generate_v4(), '${make}', '${model}', '${price}') RETURNING *;`)
+    return db.query(
+      'INSERT INTO car (car_uid, make, model, price) VALUES (uuid_generate_v4(), $1, $2, $3) RETURNING *;', 
+      [make, model, price]
+    )
   } catch (error) {
     console.error(error)
     return error.message
@@ -28,9 +34,9 @@ export const getPeople = async () => {
   }
 }
 
-export const getCar = async ({car_uid}: any) => {
+export const getCar = async ({car_uid}: {car_uid: string}) => {
   try {
-    const result = await db.query(`SELECT * FROM car WHERE car_uid = '${car_uid}';`)
+    const result = await db.query('SELECT * FROM car WHERE car_uid = $1;', [car_uid])
     return result.rows[0];
   } catch (error) {
     return error.message
