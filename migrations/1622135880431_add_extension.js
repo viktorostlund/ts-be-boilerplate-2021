d532a5e85dce35fb2d325c007d1b08d66cf9a8da
Viktor Östlund <viktorostlund@hotmail.com>
2021-05-28 13:51:11 +0200

    delete extension on exports down

--- migrations/1622135880431_add_extension.js
+++ migrations/1622135880431_add_extension.js
@@ -1,11 +1,11 @@
 exports.up = async function (knex) {
   await knex.raw(`
-  CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
   `);
 };
 
 exports.down = async function (knex) {
-	await knex.raw(`
-	
+  await knex.raw(`
+    DELETE EXTENSION IF EXISTS "uuid-ossp"
 	`);
   };
